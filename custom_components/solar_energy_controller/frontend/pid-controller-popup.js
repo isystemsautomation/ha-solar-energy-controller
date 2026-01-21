fe4598ac0d060273b2bf5277aa71f868293449a3
isystemsautomation <drezyulya.dmitry@isystemsautomation.com>
2026-01-21 13:23:08 +0200

    update

--- custom_components/solar_energy_controller/frontend/pid-controller-popup.js
+++ custom_components/solar_energy_controller/frontend/pid-controller-popup.js
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from "https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js";
-import Chart from "/local/vendor/chart.umd.min.js";
+import Chart from "./vendor/chart.umd.min.js";
 
 class PIDControllerPopup extends LitElement {
   static properties = {
